50f728f61863c04b02a64b1586ca3f5b478bd177
SrinuThala <sriniv2s2n@gmail.com>
2022-10-09 21:11:35 +0530

    Setup basic UI of App with Header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="title">📢 Git Commit History</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,12 @@
+import "./home.styles.scss";
+import Header from "../header/header.component";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
